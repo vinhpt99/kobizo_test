8689efd557d197d8345003e9977d9bf56d68b9ee
vinhpt99 <vinhpt72@gmail.com>
2025-03-20 21:37:01 +0700

    Integrate Vue.js with Symfony

---
+++ assets/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Xin chào từ Vue + Symfony!</h1>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/app.js')
+    .enableVueLoader()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction());
+
+module.exports = Encore.getWebpackConfig();
